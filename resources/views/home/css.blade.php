b36f8537a694898da954accd49467267652604c5
Tonny <tonnyblair257@gmail.com>
2024-09-26 15:44:02 +0300

    second commit

--- resources/views/home/css.blade.php
+++ resources/views/home/css.blade.php
@@ -1,24 +1,24 @@
-<link rel="stylesheet" type="text/css" href="my/css/bootstrap.min.css">
-<link rel="stylesheet" type="text/css" href="my/css/font-awesome.css">
-<link rel="stylesheet" href="my/css/templatemo-softy-pinko.css">
-<link rel="shortcut icon" href="{{ asset('home/images/favicon.png')}}" type="">
+<link rel="stylesheet" type="text/css" href="https://66f3cfa7405934ffd81b4609--marvelous-cassata-16d417.netlify.app/my/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="https://66f3cfa7405934ffd81b4609--marvelous-cassata-16d417.netlify.app/my/css/font-awesome.css">
+<link rel="stylesheet" href="https://66f3cfa7405934ffd81b4609--marvelous-cassata-16d417.netlify.app/my/css/templatemo-softy-pinko.css">
+<link rel="shortcut icon" href="https://66f3cfa7405934ffd81b4609--marvelous-cassata-16d417.netlify.app/home/images/favicon.png" type="">
 <!-- bootstrap core css -->
-<link rel="stylesheet" type="text/css" href="{{ asset('home/css/bootstrap.css')}}" />
+<link rel="stylesheet" type="text/css" href="https://66f3cfa7405934ffd81b4609--marvelous-cassata-16d417.netlify.app/home/css/bootstrap.css" />
 <!-- font awesome style -->
-<link href="{{asset('home/css/font-awesome.min.css')}}" rel="stylesheet" />
+<link href="https://66f3cfa7405934ffd81b4609--marvelous-cassata-16d417.netlify.app/home/css/font-awesome.min.css" rel="stylesheet" />
 <!-- Custom styles for this template -->
-<link href="{{ asset('home/css/style.css')}}" rel="stylesheet" />
+<link href="https://66f3cfa7405934ffd81b4609--marvelous-cassata-16d417.netlify.app/home/css/style.css" rel="stylesheet" />
 <!-- responsive style -->
-<link href="{{ asset ('home/css/responsive.css')}}" rel="stylesheet" />
+<link href="https://66f3cfa7405934ffd81b4609--marvelous-cassata-16d417.netlify.app/home/css/responsive.css" rel="stylesheet" />
 <link rel="icon" type="image/x-icon" href="{{ asset('my/logo.jpg')}}">
-<link rel="stylesheet" href="{{ asset('my/style.css')}}">
+<link rel="stylesheet" href="https://66f3cfa7405934ffd81b4609--marvelous-cassata-16d417.netlify.app/my/style.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
 <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/typed.js/2.0.11/typed.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.1/jquery.waypoints.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css"/>
-<link rel="stylesheet" href="{{asset('home/css/body.css')}}"/>
+<link rel="stylesheet" href="https://66f3cfa7405934ffd81b4609--marvelous-cassata-16d417.netlify.app/home/css/body.css"/>
 
 <style>
       .button-container {
